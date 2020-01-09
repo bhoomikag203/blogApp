6931e81ed14065d41eb475cf170ed2752dd0e44a
bhoomikag203 <bhoomikag203@gmail.com>
2020-01-09 12:37:37 +0530

    Add | Delete route

--- app.js
+++ app.js
@@ -102,6 +102,16 @@ app.put('/blogs/:id', (req, res) => {
     });
 });
 
+//delete route
+app.delete('/blogs/:id', (req, res) => {
+    Blog.findByIdAndRemove(req.params.id, (err) => {
+        if (err) {
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs');
+        }
+    });
+});
 app.listen(3000, () => {
     console.log(`Server started on port 3000`);
 });
